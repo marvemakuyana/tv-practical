3698d6c69a4cdcf285a7b9d3f10881b338461ac2
marvemakuyana <marvellousmakuyana@gmail.com>
2025-05-09 17:08:35 +0200

    updated file

--- tv-practical-full-stack-starter/fibertime_be/src/main.ts
+++ tv-practical-full-stack-starter/fibertime_be/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/');
   app.enableCors({
-    origin: 'http://localhost',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
